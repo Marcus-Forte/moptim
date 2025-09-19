9ca9ad7d160e82d59af0053e74b668dec86c909f
root <davi2812@hotmail.com>
2025-09-19 21:24:44 +0000

    fixed residual calculation before jacobian

--- include/NumericalCostSycl.hh
+++ include/NumericalCostSycl.hh
@@ -29,7 +29,7 @@ class NumericalCostSycl : public ICost<T> {
     logger_->log(ILog::Level::DEBUG, "max_work_item_dimensions: {}",
                  queue_.get_device().get_info<sycl::info::device::max_work_item_dimensions>());
 
-    logger_->log(ILog::Level::DEBUG, "param_dim: {}", param_dim);
+    logger_->log(ILog::Level::DEBUG, "Problem space: I: {}, O: {}, P: {}", input_dim_, observation_dim_, param_dim_);
 
     if (!queue_.get_device().is_cpu()) {
       input_sycl_ = sycl::malloc_device<T>(observation_dim_ * num_elements, queue_);
@@ -71,27 +71,7 @@ class NumericalCostSycl : public ICost<T> {
 
     logger_->log(ILog::Level::DEBUG, "Sycl compute cost items: {}", num_elements_);
 
-    queue_
-        .submit([&](sycl::handler& cgh) {
-          cgh.depends_on(copy_model_event);
-          auto sum = sycl::reduction<T>(cost_reduction_, 0.0, sycl::plus<T>{},
-                                        sycl::property::reduction::initialize_to_identity{});
-
-          const auto* input_capture = input_sycl_;
-          const auto* observations_capture = observations_sycl_;
-          const auto observation_dim_capture = observation_dim_;
-          auto* residual_data_capture = residual_data_;
-
-          const auto workers = sycl::range<1>(num_elements_);
-
-          cgh.parallel_for(workers, sum, [=](sycl::item<1> id, auto& reduction) {
-            const auto ItemRow = id.get_id() * observation_dim_capture;
-            Eigen::Map<VectorT> residual_map(&residual_data_capture[ItemRow], observation_dim_capture);
-            model_sycl->f(&input_capture[ItemRow], &observations_capture[ItemRow], &residual_data_capture[ItemRow]);
-            reduction += residual_map.squaredNorm();
-          });
-        })
-        .wait();
+    computeResiduals(copy_model_event, model_sycl).wait();
 
     T result;
     queue_.copy<T>(cost_reduction_, &result, 1).wait();
@@ -109,7 +89,7 @@ class NumericalCostSycl : public ICost<T> {
     Timer t0;
     t0.start();
     auto* model_sycl = sycl::malloc_device<Model>(1, queue_);
-    queue_.copy<Model>(&model, model_sycl, 1);
+    auto copy_model_event = queue_.copy<Model>(&model, model_sycl, 1);
 
     // Initialize vector of models
     std::vector<std::shared_ptr<Model>> models_plus(param_dim_);
@@ -131,46 +111,36 @@ class NumericalCostSycl : public ICost<T> {
 
     t0.start();
 
-    // // Sycl Kernel /// \todo lots of duplicate code with euler
+    const auto compute_residuals_event = computeResiduals(copy_model_event, model_sycl);
+
     auto jac_event = queue_.submit([&](sycl::handler& cgh) {
       const auto* input_capture = input_sycl_;
       const auto* observations_capture = observations_sycl_;
-      auto* cost_reduction_capture = cost_reduction_;
       auto* residual_data_capture = residual_data_;
       auto* residual_plus_data_capture = residual_plus_data_;
       auto* jacobian_data_capture = jacobian_data_;
-      const auto num_elements_capture = num_elements_;
       const auto param_dim_capture = param_dim_;
       const auto output_dim_capture = observation_dim_;
       const auto residuals_dim_capture = observation_dim_ * num_elements_;
 
-      auto sum_reduction = sycl::reduction<T>(cost_reduction_, 0.0, sycl::plus<T>{},
-                                              sycl::property::reduction::initialize_to_identity{});
+      cgh.depends_on(compute_residuals_event);
 
       const auto workers = sycl::range<2>(num_elements_, param_dim_);
 
-      cgh.parallel_for(workers, sum_reduction, [=](sycl::item<2> id, auto& sum_reduction) {
+      cgh.parallel_for(workers, [=](sycl::item<2> id) {
         const auto ItemRow = id.get_id(0) * output_dim_capture;
         const auto ItemCol = id.get_id(1);
 
+        // Computed in the previous kernel
         Eigen::Map<VectorT> residual_map(&residual_data_capture[ItemRow], output_dim_capture);
+
         Eigen::Map<VectorT> residual_plus_map(&residual_plus_data_capture[ItemRow + ItemCol * residuals_dim_capture],
                                               output_dim_capture);
         Eigen::Map<VectorT> jacobian_map(jacobian_data_capture, residuals_dim_capture, param_dim_capture);
 
-        if (ItemRow < residuals_dim_capture && ItemCol < param_dim_capture) {
-          models_sycl_plus[ItemCol].f(&input_capture[ItemRow], &observations_capture[ItemRow],
-                                      residual_plus_map.data());
+        models_sycl_plus[ItemCol].f(&input_capture[ItemRow], &observations_capture[ItemRow], residual_plus_map.data());
 
-          if (ItemCol == 0) {
-            model_sycl->f(&input_capture[ItemRow], &observations_capture[ItemRow], residual_map.data());
-            sum_reduction += residual_map.squaredNorm();
-          }
-        }
-
-        if (ItemRow < residuals_dim_capture && ItemCol < param_dim_capture) {
-          jacobian_map.block(ItemRow, ItemCol, output_dim_capture, 1) = (residual_plus_map - residual_map) / g_step;
-        }
+        jacobian_map.block(ItemRow, ItemCol, output_dim_capture, 1) = (residual_plus_map - residual_map) / g_step;
       });
     });
 
@@ -222,6 +192,24 @@ class NumericalCostSycl : public ICost<T> {
                                                           {jac_event, jtb_reset_event});
 
     sycl::event::wait({res_jtb, res_jtj});
+
+    // queue_
+    //     .submit([&](sycl::handler& cgh) {
+    //       const auto param_dim_capture = param_dim_;
+    //       cgh.depends_on({res_jtb, res_jtj});
+    //       cgh.single_task([=] {
+    //         // Copy results
+    //         for (size_t i = 0; i < param_dim_capture * param_dim_capture; ++i) {
+    //           JTJ_device[i] = (T)i;
+    //         }
+
+    //         // for (size_t i = 0; i < param_dim_; ++i) {
+    //         //   JTb[i] = JTb_device[i];
+    //         // }
+    //       });
+    //     })
+    //     .wait();
+    queue_.wait();
     stop = t0.stop();
     logger_->log(ILog::Level::DEBUG, "Sycl kernel jacobian: took: {} us", stop);
 
@@ -237,6 +225,36 @@ class NumericalCostSycl : public ICost<T> {
     sycl::free(model_sycl, queue_);
     sycl::free(JTJ_device, queue_);
     sycl::free(JTb_device, queue_);
+  }
+
+ private:
+  /**
+   * @brief Compute residuals and cost in parallel
+   *
+   * @param copy_model_event
+   * @param model_sycl
+   * @return sycl::event
+   */
+  sycl::event computeResiduals(sycl::event copy_model_event, Model* model_sycl) {
+    return queue_.submit([&](sycl::handler& cgh) {
+      cgh.depends_on(std::move(copy_model_event));
+      auto sum = sycl::reduction<T>(cost_reduction_, 0.0, sycl::plus<T>{},
+                                    sycl::property::reduction::initialize_to_identity{});
+
+      const auto* input_capture = input_sycl_;
+      const auto* observations_capture = observations_sycl_;
+      const auto observation_dim_capture = observation_dim_;
+      auto* residual_data_capture = residual_data_;
+
+      const auto workers = sycl::range<1>(num_elements_);
+
+      cgh.parallel_for(workers, sum, [=](sycl::item<1> id, auto& reduction) {
+        const auto ItemRow = id.get_id() * observation_dim_capture;
+        Eigen::Map<VectorT> residual_map(&residual_data_capture[ItemRow], observation_dim_capture);
+        model_sycl->f(&input_capture[ItemRow], &observations_capture[ItemRow], &residual_data_capture[ItemRow]);
+        reduction += residual_map.squaredNorm();
+      });
+    });
   }
 
  private:
